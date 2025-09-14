93e6f38f6406484e68b6bfab72f560f7fff7740c
pepetoma <blockchainjujisha@gmail.com>
2025-09-15 01:26:00 +0900

    chore(frontend): dart format適用とdart fixによる軽微修正
    - dart format -o write . を適用
    
    - dart fix --apply で未使用importを削除
    
    - flutter test 緑 (1件PASS)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -16,4 +16,3 @@ class SddkmApp extends StatelessWidget {
     );
   }
 }
-
--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
 import 'package:sddkm_frontend/main.dart';
 
 void main() {
@@ -8,4 +7,3 @@ void main() {
     expect(find.text('SDDKM MVP: E2E OK'), findsOneWidget);
   });
 }
-
